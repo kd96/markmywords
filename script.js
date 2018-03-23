5e7cc1cdc6d2dcd46452f69fe439718682ed1510
Karthik D <krtdvn@gmail.com>
2018-03-23 08:42:41 +0530

    deleted sw

--- script.js
+++ script.js
@@ -14,10 +14,3 @@ function update(){
     document.getElementById('preview').innerHTML = text
     document.getElementById('preview').scrollTop = t
 }
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(registration) {
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-  }).catch(function(err) {
-    console.log('ServiceWorker registration failed: ', err);
-});
-}
